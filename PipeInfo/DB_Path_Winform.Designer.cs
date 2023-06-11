d594cde1a78e06b139123778e6133d9696dff3f9
sixpeople <sixpeople1207@gmail.com>
2023-06-12 00:29:52 +0900

    데이터 베이스 경로 바깥 Class로 가져오는 Delagate 함수 구현.

--- PipeInfo/DB_Path_Winform.Designer.cs
+++ PipeInfo/DB_Path_Winform.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PipeInfo
+﻿using System.Windows.Forms;
+
+namespace PipeInfo
 {
     partial class DB_Path_Winform
     {
@@ -30,13 +32,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DB_Path_Winform));
             this.OpenFileDialog_DB = new System.Windows.Forms.OpenFileDialog();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_db_find_path = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button_db_path_ok = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_db = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,83 +46,88 @@
             // 
             this.OpenFileDialog_DB.FileName = "OpenFileDialog_DB";
             // 
-            // button1
+            // button_db_find_path
             // 
-            this.button1.Location = new System.Drawing.Point(478, 64);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "...";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_db_find_path.Location = new System.Drawing.Point(546, 80);
+            this.button_db_find_path.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_db_find_path.Name = "button_db_find_path";
+            this.button_db_find_path.Size = new System.Drawing.Size(63, 29);
+            this.button_db_find_path.TabIndex = 0;
+            this.button_db_find_path.Text = "...";
+            this.button_db_find_path.UseVisualStyleBackColor = true;
+            this.button_db_find_path.Click += new System.EventHandler(this.button_db_find_path_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button_db_path_ok);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Controls.Add(this.textBox_db);
+            this.groupBox1.Controls.Add(this.button_db_find_path);
+            this.groupBox1.Location = new System.Drawing.Point(14, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(604, 112);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(690, 140);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "File Path";
             // 
-            // button2
+            // button_db_path_ok
             // 
-            this.button2.Location = new System.Drawing.Point(539, 64);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(55, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "확인";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button_db_path_ok.Location = new System.Drawing.Point(616, 80);
+            this.button_db_path_ok.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_db_path_ok.Name = "button_db_path_ok";
+            this.button_db_path_ok.Size = new System.Drawing.Size(63, 29);
+            this.button_db_path_ok.TabIndex = 5;
+            this.button_db_path_ok.Text = "확인";
+            this.button_db_path_ok.UseVisualStyleBackColor = true;
+            this.button_db_path_ok.Click += new System.EventHandler(this.button_db_path_ok_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 37);
+            this.label3.Location = new System.Drawing.Point(17, 46);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.Size = new System.Drawing.Size(52, 15);
             this.label3.TabIndex = 4;
             this.label3.Text = "설명 : ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 69);
+            this.label2.Location = new System.Drawing.Point(17, 86);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.Size = new System.Drawing.Size(87, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "파일 경로 : ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 37);
+            this.label1.Location = new System.Drawing.Point(66, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(269, 12);
+            this.label1.Size = new System.Drawing.Size(334, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "DDWorks 설계 파일을 불러옵니다(확장자 : *.db)";
             // 
-            // textBox1
+            // textBox_db
             // 
-            this.textBox1.Location = new System.Drawing.Point(86, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(386, 21);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox_db.Location = new System.Drawing.Point(98, 81);
+            this.textBox_db.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_db.Name = "textBox_db";
+            this.textBox_db.Size = new System.Drawing.Size(441, 25);
+            this.textBox_db.TabIndex = 1;
             // 
             // DB_Path_Winform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 148);
+            this.ClientSize = new System.Drawing.Size(721, 185);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "DB_Path_Winform";
             this.Text = "제작도면";
             this.groupBox1.ResumeLayout(false);
@@ -132,12 +139,12 @@
         #endregion
 
         private System.Windows.Forms.OpenFileDialog OpenFileDialog_DB;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_db_find_path;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_db_path_ok;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
+        public TextBox textBox_db;
     }
 }
--- PipeInfo/DB_Path_Winform.cs
+++ PipeInfo/DB_Path_Winform.cs
@@ -7,31 +7,53 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace PipeInfo
 {
+    public delegate void DataGetEventHandler(string data);
     public partial class DB_Path_Winform : Form
     {
+        public DataGetEventHandler DataSendEvent;
+        public string db_path;
         public DB_Path_Winform()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void button_db_find_path_Click(object sender, EventArgs e)
         {
-            var ofd = new System.Windows.Forms.OpenFileDialog();
-            ofd.ShowDialog();
+            //db파일 경로를 변수에 넘겨준다. 
+            db_path = showFileDialog();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button_db_path_ok_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
+            MessageBox.Show(db_path);
+            this.DialogResult = DialogResult.OK;
+            DataSendEvent(db_path);
             this.Close();
         }
+        public string sendValue
+        {
+            get { return this.db_path; }
+        }
+ 
+        public string showFileDialog()
+        {
+            var ofd = new System.Windows.Forms.OpenFileDialog();
+            ofd.Filter = "DDWorks DatabaseFile(*.db)|*.db";
+            ofd.ShowDialog();
+            if (ofd.ShowDialog().ToString() == "OK")
+            {
+                textBox_db.Text = ofd.FileName;
+                return ofd.FileName;
+            }
+            else if (ofd.ShowDialog().ToString() == "CANCEL")
+            {
+                return "";
+            }
+            return "";
+        } 
     }
 }
